25403e1b2d534c26ca62a5446f531c4ccf292eb4
dualklip <ml@phoenixconnection.es>
2024-05-13 15:07:40 +0200

    Renamed SkeletonServiceProvider to CscServiceProvider
    The existing SkeletonServiceProvider has been renamed to CscServiceProvider, which aligns better with the changed namespace. The associated command and package name are also updated to maintain consistency across the service provider configuration.

--- src/CscServiceProvider.php
+++ src/CscServiceProvider.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace VendorName\Skeleton;
+namespace Dualklip\Csc;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use VendorName\Skeleton\Commands\SkeletonCommand;
+use Dualklip\Csc\Commands\CscCommand;
 
-class SkeletonServiceProvider extends PackageServiceProvider
+class CscServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
@@ -16,10 +16,9 @@ class SkeletonServiceProvider extends PackageServiceProvider
          * More info: https://github.com/spatie/laravel-package-tools
          */
         $package
-            ->name('skeleton')
+            ->name('csc')
             ->hasConfigFile()
-            ->hasViews()
             ->hasMigration('create_skeleton_table')
-            ->hasCommand(SkeletonCommand::class);
+            ->hasCommand(CscCommand::class);
     }
 }
