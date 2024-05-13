020958d6aac0f82507c39c0ab62192c6aa50d27b
dualklip <ml@phoenixconnection.es>
2024-05-13 16:24:41 +0200

    Update CscServiceProvider to include InstallCommand
    This commit modifies the CscServiceProvider class. It removes comments, alters the package name from 'laravel-csc' to 'csc', and reintroduces the InstallCommand that was previously commented out. It also chains several new methods to the InstallCommand to publish the config file, publish migrations and ask to run the migrations.

--- src/CscServiceProvider.php
+++ src/CscServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Dualklip\Csc;
 
 use Dualklip\Csc\Commands\CscCommand;
+use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -10,16 +11,16 @@ class CscServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
-            ->name('laravel-csc')
+            ->name('csc')
             ->hasConfigFile()
             ->hasMigrations(['create_cities_table', 'create_countries_table', 'create_regions_table', 'create_states_table', 'create_subregions_table'])
-            ->runsMigrations();
-        //->hasCommand(CscCommand::class);
+            ->runsMigrations()
+            ->hasInstallCommand(function(InstallCommand $command) {
+                $command
+                    ->publishConfigFile()
+                    ->publishMigrations()
+                    ->askToRunMigrations();
+            });
     }
 }
