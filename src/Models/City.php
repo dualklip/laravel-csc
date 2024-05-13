4ff7e730c9e498ae2627fef7d33bd42f27b83151
dualklip <ml@phoenixconnection.es>
2024-05-13 16:35:05 +0200

    Add new models: State, SubRegion, Country, City, and Region
    Created new Eloquent models for State, SubRegion, Country, City and Region under the namespace Dualklip\Csc\Models. Each model extends the Laravel's base Model class and specifies the respective table's structure.

---
+++ src/Models/City.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Dualklip\Csc\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class City extends Model
+{
+    protected $guarded = [];
+}
---
+++ src/Models/State.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dualklip\Csc\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class State extends Model
+{
+}
