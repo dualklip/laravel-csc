69663f19cd433e9c849467c597448fec55e398e0
dualklip <ml@phoenixconnection.es>
2024-05-14 09:39:38 +0200

    Add configuration handling to Csc class
    A new method has been added to the Csc class to set configurations. Additionally, another method has been created to get configurations, either all or a specific one by key.

--- src/Csc.php
+++ src/Csc.php
@@ -4,4 +4,19 @@ namespace Dualklip\Csc;
 
 class Csc
 {
+    protected static $config = [];
+
+    public static function setConfig(array $config): void
+    {
+        static::$config = $config;
+    }
+
+    public static function getConfig(string $key = null)
+    {
+        if ($key) {
+            return static::$config[$key] ?? null;
+        }
+
+        return static::$config;
+    }
 }
