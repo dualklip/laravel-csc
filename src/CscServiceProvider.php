37d99c1d4d545e8dccff25a42fbe7367f27fa4d2
dualklip <ml@phoenixconnection.es>
2024-05-14 11:09:42 +0200

    Refactor ServiceProvider and register seeders
    Updated ServiceProvider structure by commenting out old publishing method and invoking register bindings for City, Country, Region, and State seeders. This allows for more direct and efficient seeding process during package registration.

--- src/CscServiceProvider.php
+++ src/CscServiceProvider.php
@@ -3,6 +3,10 @@
 namespace Dualklip\Csc;
 
 use Dualklip\Csc\Commands\CscEndWithInstallerCommand;
+use Dualklip\Csc\database\seeders\CitySeeder;
+use Dualklip\Csc\database\seeders\CountrySeeder;
+use Dualklip\Csc\database\seeders\RegionSeeder;
+use Dualklip\Csc\database\seeders\StateSeeder;
 use Illuminate\Support\Facades\Artisan;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
@@ -23,23 +27,21 @@ class CscServiceProvider extends PackageServiceProvider
                     ->publishMigrations()
                     ->askToRunMigrations();
             });
-        $this->publishes([
+        /*$this->publishes([
             __DIR__ . '/database/seeders/CitySeeder.php' => database_path('seeders/CitySeeder.php'),
             __DIR__ . '/database/seeders/CountrySeeder.php' => database_path('seeders/CountrySeeder.php'),
             __DIR__ . '/database/seeders/RegionSeeder.php' => database_path('seeders/RegionSeeder.php'),
             __DIR__ . '/database/seeders/StateSeeder.php' => database_path('seeders/StateSeeder.php'),
             __DIR__ . '/database/seeders/SubregionSeeder.php' => database_path('seeders/SubregionSeeder.php'),
-        ], 'csc');
+        ], 'csc');*/
     }
 
-    public function packageBooted(): void
+    public function registeringPackage(): void
     {
-        $this->publishes([
-            __DIR__ . '/database/seeders/CitySeeder.php' => database_path('seeders/CitySeeder.php'),
-            __DIR__ . '/database/seeders/CountrySeeder.php' => database_path('seeders/CountrySeeder.php'),
-            __DIR__ . '/database/seeders/RegionSeeder.php' => database_path('seeders/RegionSeeder.php'),
-            __DIR__ . '/database/seeders/StateSeeder.php' => database_path('seeders/StateSeeder.php'),
-            __DIR__ . '/database/seeders/SubregionSeeder.php' => database_path('seeders/SubregionSeeder.php'),
-        ], 'csc');
+        $this->app->bind(CitySeeder::class);
+        $this->app->bind(CountrySeeder::class);
+        $this->app->bind(RegionSeeder::class);
+        $this->app->bind(StateSeeder::class);
+        $this->app->bind(RegionSeeder::class);
     }
 }
