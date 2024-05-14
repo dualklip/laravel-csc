f2695c66baac7671b54d5611b5c0e8f84f17b814
dualklip <ml@phoenixconnection.es>
2024-05-14 10:45:14 +0200

    Simplify CSCServiceProvider and remove seeding operations
    This commit significantly simplifies the CscServiceProvider by removing the seeding operations. This includes the removal of commands for seeding regions, subregions, countries, states, and cities. This change results in a more streamlined service provider.

--- src/CscServiceProvider.php
+++ src/CscServiceProvider.php
@@ -21,28 +21,7 @@ class CscServiceProvider extends PackageServiceProvider
                 $command
                     ->publishConfigFile()
                     ->publishMigrations()
-                    ->publish()
-                    ->askToRunMigrations()
-                    ->endWith(function (InstallCommand $command) {
-                        $command->info('Installing laravel CSC...');
-
-
-                        // publish migrations
-                        $command->call('vendor:publish --tag=laravel-csc --force');
-                        // migrate new tables
-                        $command->call('migrate');
-                        // re-seed the world data
-                        $command->info('seeding regions...');
-                        $command->call('db:seed --class=RegionSeeder');
-                        $command->info('seeding subregions...');
-                        $command->call('db:seed --class=SubregionSeeder');
-                        $command->info('seeding countries...');
-                        $command->call('db:seed --class=CountrySeeder');
-                        $command->info('seeding states...');
-                        $command->call('db:seed --class=StateSeeder');
-                        $command->info('seeding cities...');
-                        $command->call('db:seed --class=CitySeeder');
-                    });
+                    ->askToRunMigrations();
             });
     }
 
